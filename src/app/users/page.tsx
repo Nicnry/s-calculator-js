de4d1de6f9aae482150946c7550c878f4e350fdc
Nicolas <m.nicolas.henry@gmail.com>
2025-03-04 17:56:18 +0100

    Refresh route /users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -41,6 +41,5 @@ async function UserListWrapper() {
   return <UserList users={users} />;
 }
 
-export const revalidate = 60;
-
+export const dynamic = "force-dynamic";
 export default UsersPageLayout;
