565040f81921555f716b702bd433c08dd9c65cf7
Nicolas <m.nicolas.henry@gmail.com>
2025-03-19 22:47:50 +0100

    gitflow-hotfix-stash: fix-mobile-form

--- src/app/components/accounts/accountForm.tsx
+++ src/app/components/accounts/accountForm.tsx
@@ -14,7 +14,7 @@ export default function AccountForm({ userId, account, update = false }: { userI
     { name: "bankName", label: "Nom banque", placeholder: "UBS", icon: <Banknote /> },
     { name: "accountNumber", label: "No compte", placeholder: "CH1589144187277187766", icon: <Hash /> },
     { name: "accountType", label: "Type de compte", type: "select", options: ["Courant", "Épargne"], icon: <Wallet /> },
-    { name: "balance", label: "Montant", type: "number", value: "1000", icon: <DollarSign /> },
+    { name: "balance", label: "Montant", type: "number", icon: <DollarSign /> },
     { name: "currency", label: "Monnaie", placeholder: "CHF", icon: <Coins /> },
   ];
 
